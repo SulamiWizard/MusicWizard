a6d900904a183424400560e35ab39065834070be
SulamiWizard <SulamiWizard@gmail.com>
2024-08-23 18:42:23 -0700

    Removed 3 commands and implemented an example slash command

--- main.py
+++ main.py
@@ -4,33 +4,34 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+
+def is_valid_link(link):
+    pass
+
+
 load_dotenv()
 
 TOKEN = str(os.getenv("DISCORD_TOKEN"))
 
-client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 
 @client.event
 async def on_ready():
+    await client.tree.sync()
     print("Success: Bot is connected to Discord")
 
 
-@client.command()
-async def ping(ctx):
-    await ctx.send("Pong")
+# example slash command
+@client.tree.command(name="ping", description="Shows the bot's latency in ms.")
+async def ping(interaction: discord.Interaction):
+    bot_latency = round(client.latency * 1000)
+    await interaction.response.send_message(f"Pong! {bot_latency}")
 
 
 @client.command()
-async def magic_eightball(ctx, *, question):
-    pass
+async def test(ctx, args):
+    await ctx.send(f"Pong {args}")
 
-@client.command()
-async def bruh(ctx):
-    await ctx.send("bruv")
-
-@client.command()
-async def ok(ctx):
-    await ctx.send("yes")
 
 client.run(TOKEN)
